699fb4594b468c0e4f84236f3335aba59165f9a5
neemiasRamos <41588565+neemiasRamos@users.noreply.github.com>
2018-07-23 18:02:10 -0300

    Create cores.cpp

---
+++ cpp/Aleatorios/cores.cpp
@@ -0,0 +1,18 @@
+#include<conio.h>
+#include<stdio.h>
+
+main()
+{
+    int i;
+   
+   
+   for(i=0;i<16;i++)
+    {
+      textcolor(i);
+      printf("\ncores do texto= %d",i);
+      }
+      //colocando sinal de mais
+         
+      
+ getch();     
+}
