da3738ccb6d0dcd37ff9f6c0346b03c38f57ef49
neemiasRamos <41588565+neemiasRamos@users.noreply.github.com>
2018-07-23 16:15:33 -0300

    Create EX01.cpp

---
+++ cpp/EX01.cpp
@@ -0,0 +1,21 @@
+/*
+	Name: SomaSeDez.cpp
+	Author: Neemias Ramos Ferreira
+	Date: 12/08/15 18:36
+	Description: Calcule a soma de dois números, se o resultado for maior que 10, exiba ele.
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int a, b;
+	printf("Digite um valor: ");
+	scanf("%d", &a);
+	printf("Digite um valor: ");
+	scanf("%d", &b);
+	if((a+b)>10)
+		printf("O resultado da soma eh: %d", a+b);
+	getch();
+}
