3232f3785b76874ad5b380dcfe31fceb0985a8fd
neemiasRamos <41588565+neemiasRamos@users.noreply.github.com>
2018-07-23 17:09:13 -0300

    Update and rename exercicio 14, funcao troca valores de duas variaveis.cpp to EX14.cpp

---
+++ cpp/funcoes/EX14.cpp
@@ -0,0 +1,42 @@
+/*
+  Author: Neemias Ramos Ferreira
+  Date: 03/09/15 18:11
+  Description: Escreva um procedimento que receba como parâmetro dois valores numéricos e ao final de sua execução 
+  troque os valores entre si. Por exemplo, suponhamos que primeiro parâmetro tenha recebido o valor 10 e o segundo parâmetro
+   o valor 20, ao final da execução, a variável que representa o primeiro parâmetro deverá conter o valor 20 e a 
+variável que representa o segundo parâmetro deverá conter o valor 10 (utiliza passagem de parâmetros por referência).
+        
+*/
+#include<conio.h>
+#include<stdio.h>
+
+int lernum()
+{
+    int x;
+    scanf("%d", &x);
+    return x;
+    
+}
+
+int idade(int a, int b)
+{
+  int aux;
+   aux=a;
+   a=b;
+   b=aux;
+   printf("ordem inversa é igual a: %d, %d\n",a,b);
+ 
+}
+
+main()
+{
+    int a,b;
+    puts("digite a dois valores\n");
+    a=lernum();
+    b=lernum();
+    printf("a ordem digitada é igual a: %d, %d\n", a,b);
+    idade(a,b);
+    
+      
+getch();      
+}
