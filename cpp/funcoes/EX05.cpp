07c9669c5a26e6c1ec5909fe109a83748b0d5349
neemiasRamos <41588565+neemiasRamos@users.noreply.github.com>
2018-07-23 17:27:47 -0300

    Create EX05.cpp

---
+++ cpp/funcoes/EX05.cpp
@@ -0,0 +1,32 @@
+/*
+  Author: Neemias Ramos Ferreira
+  Date: 18/09/15 19:59
+  Description: Escreva um programa para calcular o índice de massa corporal de um sujeito (IMC). 
+  Para calcular o IMC deve se dividir o peso (em Kg) pelo quadrado da altura (em metros). 
+  Ao final o algoritmo deve apresentar a seguinte interpretação do resultado: abaixo do peso (IMC menor que 18,5),
+   peso normal (IMC entre 18,5 e 24,99),acima do peso (IMC entre 25 e 30) e obeso (IMC acima de 30).
+*/
+#include<conio.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+
+float imc()
+{
+      int peso;
+      float altura;
+      
+      printf("digite o seu peso");
+      scanf("%d",&peso);
+      printf("digite a sua altura");
+      scanf("%f",altura);
+      imc
+      return peso,altura;
+}
+main()
+{
+    imc();
+    
+      
+getch();
+}
