c336ba969798991cdbd191b5cea6ab1dbb0ccbc3
neemiasRamos <neemias.ferreira@fatec.sp.gov.br>
2018-07-23 19:49:55 -0300

    Create Aluno.java

---
+++ POO/ProjetoEscola/Aluno.java
@@ -0,0 +1,54 @@
+
+/**
+ * Classe para
+ * @author Neemias Ramos Ferreira
+ * @since classe criada em 01/04/2016
+ */
+public class Aluno {
+    
+    private String codigo;
+    private String nome;
+    private String curso;
+    
+    
+    /**
+     * @return the codigo
+     */
+    public String getCodigo() {
+        return codigo;
+    }
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo (String codigo) {
+        this.codigo = codigo;
+    }
+    
+    /** 
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }    
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome( String nome) {
+        this.nome = nome;
+    }
+    
+    
+    /**
+     * @return the curso
+     */
+    public String getCurso() {
+        return curso;
+    }
+    /**
+     * @param curso the curso to set
+     */
+    public void setCurso (String curso) {
+        this.curso = curso;
+    }
+    
+}//fim da classe
