5e8f3a170b84ada09111d2b86bb7501d276c4651
neemiasRamos <neemias.ferreira@fatec.sp.gov.br>
2018-07-23 19:47:07 -0300

    Create TemperaturaException.java

---
+++ POO/temperatura/TemperaturaException.java
@@ -0,0 +1,11 @@
+
+import javax.swing.JOptionPane;
+
+class TemperaturaException extends Exception {
+
+    public TemperaturaException(String msg) {
+        
+        super (msg);
+    }
+
+}//Fim da Classe!!!
