828bbcc287c8ac35df05f1e7af66912ba104fb67
neemiasRamos <neemias.ferreira@fatec.sp.gov.br>
2018-07-23 19:54:14 -0300

    Create Curso.java

---
+++ POO/ProjetoEscola/Curso/Curso.java
@@ -0,0 +1,44 @@
+
+package Curso;
+
+/**
+ * Classe para
+ * @author Neemias Ramos Ferreira
+ * @since classe criada em 15/04/2016
+ */
+class Curso {
+ private String codcurso;
+ private String descricao;
+    
+    
+     /**
+     * @return the codcurso
+     */
+    public String getCodcurso() {
+        return codcurso;
+    }
+    /**
+     * @param codcurso the codigo to set
+     */
+    public void setCodcurso (String codcurso) {
+        this.codcurso = codcurso;
+    }
+    
+    
+     /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+    /**
+     * @param descricao the codigo to set
+     */
+    public void setDescricao (String descricao) {
+        this.descricao = descricao;
+    }
+    
+    
+
+}//fim da classe
+
