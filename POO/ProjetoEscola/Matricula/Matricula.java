5a034ecdee4cab0292a59151688b8c2142a3b329
neemiasRamos <neemias.ferreira@fatec.sp.gov.br>
2018-07-23 19:57:19 -0300

    Create Matricula.java

---
+++ POO/ProjetoEscola/Matricula/Matricula.java
@@ -0,0 +1,56 @@
+package Matricula;
+
+/**
+ * Classe para
+ *
+ * @author Neemias Ramos Ferreira
+ * @since classe criada em 15/04/2016
+ */
+public class Matricula {
+
+    private String codmatricula;
+    private String nomeal;
+    private String curso;
+
+    /**
+     * @return the codmatricula
+     */
+    public String getCodMatricula() {
+        return codmatricula;
+    }
+
+    /**
+     * @param codmatricula the codigo to set
+     */
+    public void setCodMatricula(String codmatricula) {
+        this.codmatricula = codmatricula;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getNomeal() {
+        return nomeal;
+    }
+
+    /**
+     * @param nomeal the codigo to set
+     */
+    public void setNomeal(String nomeal) {
+        this.nomeal = nomeal;
+    }
+
+    /**
+     * @return the curso
+     */
+    public String getCurso() {
+        return curso;
+    }
+    /**
+     * @param curso the codigo to set
+     */
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+}//fim da classe
