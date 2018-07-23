ee58177931f796a343a1eb348ce6e14e5c1e83a9
neemiasRamos <41588565+neemiasRamos@users.noreply.github.com>
2018-07-23 17:46:25 -0300

    Create ponteiros.cpp

---
+++ cpp/Aleatorios/ponteiros.cpp
@@ -0,0 +1,21 @@
+/* 
+  Author: Neemias Ramos Ferreira 
+  Date: 02/10/15 20:20
+  Description: 
+*/
+#include<conio.h>
+#include<stdio.h>
+
+main()
+{
+  int i,j,k;//
+ 
+  printf("endereco de i=%p\n",&i);//p para ponteiros
+   printf("endereco de j=%p\n",&j);//p para ponteiros
+    printf("endereco de k=%p\n",&k);//p para ponteiros
+  getch();    
+      
+}
+//& endereco 
+// conteudo de onde esta apontado
+// quando nao tem nada mostra o que tem la dentro
